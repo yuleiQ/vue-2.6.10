c5552d702711b00732a943e93d8417f6746067c6
qiyulei <qiyulei@gridsum.com>
2020-08-03 23:29:33 +0800

    first

---
+++ src/core/observer/array.js
@@ -0,0 +1,50 @@
+/*
+ * not type checking this file because flow doesn't play well with
+ * dynamically accessing methods on Array prototype
+ */
+
+import { def } from '../util/index'
+// 获取数组原型
+const arrayProto = Array.prototype
+export const arrayMethods = Object.create(arrayProto)
+
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+]
+
+/**
+ * Intercept mutating methods and emit events
+ */
+ //  覆盖7个方法
+methodsToPatch.forEach(function (method) {
+  // cache original method
+  const original = arrayProto[method]
+  def(arrayMethods, method, function mutator (...args) {
+    // 执行原定的任务
+    const result = original.apply(this, args)
+    // 通知
+    const ob = this.__ob__
+    // 插入操作，做响应化处理
+    let inserted
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args
+        break
+      case 'splice':
+        inserted = args.slice(2)
+        break
+    }
+    if (inserted) ob.observeArray(inserted)
+    // notify change
+    // 通知更新
+    ob.dep.notify()
+    return result
+  })
+})
